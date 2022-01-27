e49ed5fc75e6a56185831458a8503210d084b80e
NonExistantUsername <mayor4090@gmail.com>
2022-01-27 14:33:30 +0200

    Controller interface update

--- include/general/interfaces/controller.hpp
+++ include/general/interfaces/controller.hpp
@@ -1,9 +1,14 @@
 #pragma once
 
 class IRenderer;
+class Application;
 
 class IController {
+protected:
+    Application* app = nullptr;
 public:
+    IController(Application* app): app(app) {}
+
     virtual void update() = 0;
     virtual void draw(IRenderer* renderer) = 0;
 };
