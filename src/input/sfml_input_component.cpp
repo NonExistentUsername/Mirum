da0bbeb7ea8582759cb54e79ad12be228f389fdf
NonExistantUsername <mayor4090@gmail.com>
2022-01-29 17:35:28 +0200

    Fix

--- src/input/sfml_input_component.cpp
+++ src/input/sfml_input_component.cpp
@@ -1,14 +1,13 @@
 #include "input/sfml_input_component.hpp"
 #include "render/sfml_renderer.hpp"
-#include "input/ievent_handler.hpp"
 
 SFMLInputComponent::SFMLInputComponent(sf::RenderWindow* window):
     window(window) {}
 
-void SFMLInputComponent::handleEvents(IEventHandler* command) {
+void SFMLInputComponent::handleEvents(_weak_observer_ptr handler) {
     sf::Event event;
 
     while (window->pollEvent(event)) {
-        command->handle(event);
+        handler->notify(event);
     }
 }
