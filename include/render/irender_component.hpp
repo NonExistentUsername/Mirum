50a4dd7d3beebe6906ff5105c06ea1c56670c892
NonExistantUsername <mayor4090@gmail.com>
2022-02-01 11:26:58 +0200

    Fix

--- include/render/irender_component.hpp
+++ include/render/irender_component.hpp
@@ -1,8 +1,10 @@
 #pragma once
 
+class IRenderer;
+
 class IRenderComponent {
 private:
     using _weak_renderer_ptr = IRenderer*;
 public:
     virtual void draw(_weak_renderer_ptr renderer) = 0;
-};
+}; 
