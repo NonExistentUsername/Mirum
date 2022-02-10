8352016d6b0816ab9dcd47716e857894ab5ffd25
NonExistantUsername <mayor4090@gmail.com>
2022-02-10 13:26:09 +0200

    Added math vector

---
+++ include/math/vec.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+
+template<class T>
+class Vec {
+    T x, y;
+
+    Vec() {}
+    Vec(const Vec<T>& vec):
+        x(x), y(y) {}
+    Vec(T x, T y):
+        x(std::move(x)), y(std::move(y)) {}
+    Vec(sf::Vector2<T> vec):
+        x(std::move(x)), y(std::move(y)) {} 
+};
